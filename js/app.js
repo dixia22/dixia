d1bbd51e7ae6d9a5acc09fa7aa89d9c28eac6dd4
dixia22 <138125321+dixia22@users.noreply.github.com>
2023-07-06 22:29:44 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,90 @@
+
+const menu = document.querySelector('.navegacionn');
+const navegacion = document.querySelector('.navegacion');
+const imagenes = document.querySelectorAll('img');
+const btnTodos = document.querySelector('.todos');
+
+document.addEventListener('DOMContentLoaded',()=>{
+    eventos();
+    platillos();
+});
+
+const eventos = () =>{
+    menu.addEventListener('click',abrirMenu);
+}
+
+const abrirMenu = () =>{
+     navegacion.classList.remove('ocultar');
+     botonCerrar();
+}
+
+const botonCerrar = () =>{
+    const btnCerrar = document.createElement('p');
+    const overlay  = document.createElement('div');
+    overlay.classList.add('pantalla-completa');
+    const body = document.querySelector('body');
+    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
+    body.appendChild(overlay);
+    btnCerrar.textContent = 'x';
+    btnCerrar.classList.add('btn-cerrar');
+
+    // while(navegacion.children[5]){
+    //     navegacion.removeChild(navegacion.children[5]);
+    // }
+    navegacion.appendChild(btnCerrar);   
+    cerrarMenu(btnCerrar,overlay);
+    
+}
+
+const observer = new IntersectionObserver((entries, observer)=>{
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+                const imagen = entry.target;
+               
+                
+            }
+        }); 
+});
+
+
+imagenes.forEach(imagen=>{
+   
+    observer.observe(imagen);
+});
+
+const cerrarMenu = (boton, overlay) =>{
+    boton.addEventListener('click',()=>{
+        navegacion.classList.add('ocultar');
+        overlay.remove();
+        boton.remove();
+    });
+
+    overlay.onclick = function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar');  
+        boton.remove();
+    }
+}
+
+
+//Función para filtrar las categorías de los trabajos
+function verCategoria(cat){
+    const items = document.getElementsByClassName("item");
+    for(let i=0; i < items.length;i++){
+        items[i].style.display = "none";
+    }
+
+    const itemCat = document.getElementsByClassName(cat);
+    for(let i = 0; i<itemCat.length;i++){
+        itemCat[i].style.display = "block";
+    }
+
+    const links = document.querySelectorAll(".trabajos nav a");
+    links[0].className = "";
+    links[1].className = "";
+    links[2].className = "";
+    links[3].className = "";
+
+    const itemSeleccionado = document.getElementById(cat);
+    itemSeleccionado.className = "borde";
+}
